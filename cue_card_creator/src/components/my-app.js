c7cd0836e76fa1f71721bcd60f5ff325ee15740f
kareemhageali <kareem.hageali@gmail.com>
2019-08-14 00:13:12 -0400

    Add card creator component

--- cue_card_creator/src/components/my-app.js
+++ cue_card_creator/src/components/my-app.js
@@ -3,14 +3,13 @@ import { setPassiveTouchGestures } from '@polymer/polymer/lib/utils/settings.js'
 import { connect } from 'pwa-helpers/connect-mixin.js';
 import { installRouter } from 'pwa-helpers/router.js';
 
-import { store } from '../store.js';
-
-import { navigate } from '../actions/app.js';
-
 import '@polymer/app-layout/app-drawer/app-drawer.js';
 import '@polymer/app-layout/app-header/app-header.js';
 import '@polymer/app-layout/app-scroll-effects/effects/waterfall.js';
 import '@polymer/app-layout/app-toolbar/app-toolbar.js';
+
+import { store } from '../store.js';
+import { navigate } from '../actions/app.js';
 
 class MyApp extends connect(store)(LitElement) {
   static get properties() {
@@ -26,8 +25,6 @@ class MyApp extends connect(store)(LitElement) {
         :host {
           display: block;
 
-          --app-drawer-width: 256px;
-
           --app-primary-color: #E91E63;
           --app-secondary-color: #293237;
           --app-dark-text-color: var(--app-secondary-color);
@@ -35,24 +32,23 @@ class MyApp extends connect(store)(LitElement) {
           --app-section-even-color: #f7f7f7;
           --app-section-odd-color: white;
 
-          --app-header-background-color: white;
-          --app-header-text-color: var(--app-dark-text-color);
-          --app-header-selected-color: var(--app-primary-color);
-
-          --app-drawer-background-color: var(--app-secondary-color);
-          --app-drawer-text-color: var(--app-light-text-color);
-          --app-drawer-selected-color: #78909C;
+          --paper-button-primary-color: #1dbf22;
+          --paper-button-text-color: white;
         }
 
-        app-header {
-          position: fixed;
-          top: 0;
-          left: 0;
+        .main-content {
+          padding-top: 64px;
+        }
+
+        .page {
+          display: none;
+        }
+
+        .page[active] {
+          display: flex;
+          flex-direction: column;
           width: 100%;
-          text-align: center;
-          background-color: var(--app-header-background-color);
-          color: var(--app-header-text-color);
-          border-bottom: 1px solid #eee;
+          height: 100%;
         }
       }
       `
@@ -61,39 +57,9 @@ class MyApp extends connect(store)(LitElement) {
 
   render() {
     return html`
-      <!-- Header -->
-      <app-header condenses reveals effects="waterfall">
-        <app-toolbar class="toolbar-top">
-          <button class="menu-btn" title="Menu"></button>
-          <div main-title>${this.appTitle}</div>
-        </app-toolbar>
-
-        <!-- This gets hidden on a small screen-->
-        <nav class="toolbar-list">
-          <a ?selected="${this._page === 'view1'}" href="/view1">View One</a>
-          <a ?selected="${this._page === 'view2'}" href="/view2">View Two</a>
-          <a ?selected="${this._page === 'view3'}" href="/view3">View Three</a>
-        </nav>
-      </app-header>
-
-      <!-- Drawer content -->
-      <app-drawer
-          .opened="${this._drawerOpened}"
-          @opened-changed="${this._drawerOpenedChanged}">
-        <nav class="drawer-list">
-          <a ?selected="${this._page === 'view1'}" href="/view1">View One</a>
-          <a ?selected="${this._page === 'view2'}" href="/view2">View Two</a>
-          <a ?selected="${this._page === 'view3'}" href="/view3">View Three</a>
-        </nav>
-      </app-drawer>
-
-      <!-- Main content -->
-      <!-- <main role="main" class="main-content">
-        <my-view1 class="page" ?active="${this._page === 'view1'}"></my-view1>
-        <my-view2 class="page" ?active="${this._page === 'view2'}"></my-view2>
-        <my-view3 class="page" ?active="${this._page === 'view3'}"></my-view3>
-        <my-view404 class="page" ?active="${this._page === 'view404'}"></my-view404>
-      </main> -->
+      <main role="main" class="main-content">
+        <card-creator class="page" ?active="${this._page === 'create'}"></card-creator>
+      </main>
     `;
   }
 
