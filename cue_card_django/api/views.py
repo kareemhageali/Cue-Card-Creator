69a515b6f81ffda18b34dde9470b9312455a42ea
kareemhageali <kareem.hageali@gmail.com>
2019-08-15 00:07:40 -0400

    Set up CardViewSet with django rest framework

--- cue_card_django/api/views.py
+++ cue_card_django/api/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from api.models import Card
+from api.serializers import CardSerializer
+
+
+class CardViewSet(viewsets.ModelViewSet):
+    queryset = Card.objects.all()
+    serializer_class = CardSerializer
